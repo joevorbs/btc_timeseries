c592f64b9be38752cf6ec385412911278a367678
joevorbs <jvorbeck1@gmail.com>
2021-05-26 14:44:21 -0400

    added prophet code to obtain residuals, code to reverse dataset'
    fgfg

--- multivariate_forecasting.py
+++ multivariate_forecasting.py
@@ -15,6 +15,9 @@ import time
 #Current date
 today = datetime.now().date()
 
+#Amount of time to pre-lag data in terms of days
+lags = 730
+
 #Read in analytical dataset comprised of blockchain and bitcoin data
 btc = pd.read_csv("btc_analytical_dataset" + str(today) + ".csv")
 
@@ -28,32 +31,70 @@ btc['Timestamp'] = pd.to_datetime(btc['Timestamp'], yearfirst = True)
 #Can use a timeseries based method to interpolate missing values - imputing averages won't work in BTC case
 btc_interp = btc.set_index("Timestamp").interpolate("time", axis = 0)
 
-#Initialize empty df
+#Drop rows were interpolation cant fill rows & reset index so timestamp is used as a column
+btc_interp = btc_interp.dropna().reset_index()
+
+#Rename timestamp for prophet
+btc_interp.rename(columns = {'Timestamp' : 'ds'}, inplace = True)
+
+#Empty dataframe to store residuals 
+btc_residuals = pd.DataFrame()
+
+#List of columns to iterate through - ignoring our target variable and date
+col_list = new_df.columns.remove["Closing Price (USD)", "ds"]]
+
+#Loop for all variables minus the target and obtain their residuals
+for i in col_list:
+    ts = Prophet(daily_seasonality = False)
+    cols_to_use = btc_interp[["ds", i]].rename(columns = {i : "y"})
+    ts.fit(cols_to_use)
+    preds = ts.predict(cols_to_use)
+    btc_residuals[i] = cols_to_use['y'] - preds['yhat']
+
+#Initialize empty df to store leads
 new_df = pd.DataFrame()
 
 #Cutoff for lead times in days - if every lead is included we will have a one row dataset
-cutoff = range(0, 1000)
+cutoff = range(0, lags) #2 Years 
 
-#For every column, create a new column based off every single possible lead time and append to a blank df
-for i in btc_interp.columns:
+#Need to reverse dataset order to place most recent observations at the top
+btc_residuals_flip = btc_residuals.iloc[::-1]
+
+#For every column, create a new column based off every single possible lead time in the specified range and append to a blank df
+for i in btc_residuals_flip.columns:
     for j in cutoff:
-        new_col = btc_interp[i].shift(j)
+        new_col = btc_residuals_flip[i].shift(j)
         new_df[str(i) + "_" + str(j)] = new_col  #Name of each column is the column name with the lead time as a suffix
 
 #Trim off rows with subsequent missing values
 new_df.dropna(inplace = True)
 
+#Flip dataset back to original order
+new_df = new_df.iloc[::-1]
+
+#Reset index to append target back to dataset - will try to match up rows based on the index
+new_df = new_df.reset_index().drop("index", axis = 1)
+
+#Append target back to dataframe  of led features - will be (N - L; number of rows - leads)
+new_df['Closing Price (USD)'] = btc_interp['Closing Price (USD)'][0:len(new_df)]
+
 #Isolate features & target
-X = final_df.drop("Closing Price (USD)_0", axis = 1)
-y = final_df['Closing Price (USD)_0']
+X = final_df.drop("Closing Price (USD)", axis = 1)
+y = final_df['Closing Price (USD)']
 
-#Train/test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .25)
+#Train/test split - want to keep 2/3 of the data for training, 1/3 for testing
+X_train = X[0:round(len(X) * .66)]
+X_test = X[len(X_train):]
 
-#Initialize lasso regression - regularization will help eliminate any non-signifcant lead times
-lasso_cv = LassoCV(cv = 5,
-                   n_alphas = 100) 
+y_train = y[0:round(len(y) * .66)]
+y_test = y[len(y_train):]
 
-#Initialize ordered lasso regresssion - alpha set to 1
-ord_lasso = OrdinalRidge(alpha = 1)
+#Initialize lasso regression - regularization will help eliminate any non-signifcant lead times - can use CV if data is prewhitened
+lasso_cv = LassoCV(cv = 5, n_alphas = 100, max_iter = 1000) 
 
+#Obtain coefficients 
+coefs = pd.DataFrame(list(zip(lasso_cv.coef_, X_train.columns))).rename(columns = {0 : "Beta", 1 : "Feature"})
+#Filter out any beta values shrunk to 0
+coefs_final = coefs[coefs["Beta"] > 0]
+
+##Build model with features chosen by regularization
