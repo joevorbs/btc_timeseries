710f2ce988aafb96a5aaa6c429566f075db2a47b
joevorbs <jvorbeck1@gmail.com>
2021-05-28 18:25:07 -0400

    globalized variables and added code to store selected features from lasso

--- multivariate_feature_selection.py
+++ multivariate_feature_selection.py
@@ -15,8 +15,17 @@ import time
 #Current date
 today = datetime.now().date()
 
-#Amount of time to pre-lag data in terms of days
-lags = 730
+#Minimum and maxium lead times (in days)
+min_leads, max_leads = [183,730] #About half a year to 2 years
+
+#Cutoff for lead times in days - if every lead is included we will have a one row dataset
+cutoff = range(min_leads, max_leads)
+
+#Target for variable selection
+target = "Closing Price (USD)"
+
+#Path to save to
+path = "/Users/joevorbeck/Documents/btc_timeseries/multivariate_feature_list/"
 
 #Read in analytical dataset comprised of blockchain and bitcoin data
 btc = pd.read_csv("btc_analytical_dataset" + str(today) + ".csv")
@@ -41,7 +50,7 @@ btc_interp.rename(columns = {'Timestamp' : 'ds'}, inplace = True)
 btc_residuals = pd.DataFrame()
 
 #List of columns to iterate through - ignoring our target variable and date
-col_list = new_df.columns.remove["Closing Price (USD)", "ds"]]
+col_list = new_df.columns.remove[target, "ds"]]
 
 #Loop for all variables minus the target and obtain their residuals
 for i in col_list:
@@ -76,11 +85,11 @@ new_df = new_df.iloc[::-1]
 new_df = new_df.reset_index().drop("index", axis = 1)
 
 #Append target back to dataframe  of led features - will be (N - L; number of rows - leads)
-new_df['Closing Price (USD)'] = btc_interp['Closing Price (USD)'][0:len(new_df)]
+new_df["Target"] = btc_interp[target][0:len(new_df)]
 
 #Isolate features & target
-X = final_df.drop("Closing Price (USD)", axis = 1)
-y = final_df['Closing Price (USD)']
+X = final_df.drop(target, axis = 1)
+y = final_df[target]
 
 #Train/test split - want to keep 2/3 of the data for training, 1/3 for testing
 X_train = X[0:round(len(X) * .66)]
@@ -90,11 +99,16 @@ y_train = y[0:round(len(y) * .66)]
 y_test = y[len(y_train):]
 
 #Initialize lasso regression - regularization will help eliminate any non-signifcant lead times - can use CV if data is prewhitened
-lasso_cv = LassoCV(cv = 5, n_alphas = 100, max_iter = 1000) 
+lasso_cv = LassoCV(cv = 5, n_alphas = 100, max_iter = 1000, normalize = True) 
 
-#Obtain coefficients 
+#Fit model to training data
+lasso_cv.fit(X_train, y_train)
+#Predict on test set and assess model
+lasso_cv.predict(X_test)
+
+#Get coefficients from model paired with names
 coefs = pd.DataFrame(list(zip(lasso_cv.coef_, X_train.columns))).rename(columns = {0 : "Beta", 1 : "Feature"})
-#Filter out any beta values shrunk to 0
-coefs_final = coefs[coefs["Beta"] > 0]
-
-##Build model with features chosen by regularization
+#Filter any coefficients shrunk to 0
+coefs_final_list = coefs[coefs['Beta'] > 0]
+#Write out list of features for the forecasting model
+coefs_final_list['Variable'].to_csv(path + "feature_list_" + str(today) + ".csv")
