256afff232dcc7d3a66b8d053b43b54893f0ac42
joevorbs <jvorbeck1@gmail.com>
2021-05-30 21:58:47 -0400

    added imports, changed paths, cleanup

--- aws_prod/multivariate_forecast.py
+++ aws_prod/multivariate_forecast.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from datetime import date
 import time
+import boto3
+import s3fs
 
 #Current date
 today = datetime.now().date()
@@ -20,20 +22,33 @@ cutoff = range(min_leads, max_leads)
 target = "Closing Price (USD)"
 
 #Path to obtain model features
-path = "/Users/joevorbeck/Documents/btc_timeseries/multivariate_feature_list/"
+path = "/home/ec2-user/repos/btc_timeseries/aws_prod/multivariate_feature_list/"
 
 #Read in analytical dataset comprised of blockchain and bitcoin data
-btc = pd.read_csv("btc_analytical_dataset" + str(today) + ".csv")
+btc = pd.read_csv("s3://analytical-datasets/btc_analytical_dataset" + str(today) + "csv")
 
 #Read in list of features
 features_list = pd.read_csv(path + "feature_list_" + str(today) + ".csv")
 
-#Only include features chosen by the prior lasso 
-features_list_trimmed = set([i[:-2] for i in features_list['Variable'].values]) #Trim off lead time suffix to use a filter on normal col names
+#Only include features chosen by the prior lasso
+###need to make the trimming programmatic
+features_list_trimmed = set([i[:-4] for i in features_list['Feature'].values]) #Trim off lead time suffix to use a filter on normal col names
 btc_filtered = btc[[*features_list_trimmed]]
 
+#Convert Timestamp to datetime - need for interpolation
+btc_filtered['Timestamp'] = pd.to_datetime(btc['Timestamp'], yearfirst = True)
+
+#Interpolate missing values - we have BTC price data for everyday but not every blockchain metric is tracked everyday
+#Can use a timeseries based method to interpolate missing values - imputing averages won't work in BTC case
+btc_interp = btc_filtered.set_index("Timestamp").interpolate("time", axis = 0)
+
+#Drop rows were interpolation cant fill rows & reset index so timestamp is used as a column
+btc_interp = btc_interp.dropna().reset_index()
+
 #Reverse the order of the dataset to create leads
-btc_filtered_reverse = btc_filtered.iloc[::-1]
+btc_filtered_reverse = btc_interp.iloc[::-1]
+#Drop timestamp
+btc_filtered_reverse.drop("Timestamp", axis =  1, inplace = True)
 
 #Create empty df to store led dataframe
 btc_ml_df = pd.DataFrame()
@@ -57,8 +72,8 @@ btc_ml_df_final = btc_ml_df.filter(features_list["Feature"].values)
 btc_ml_df_final[target] = btc[target][0:len(btc_ml_df_final)]
 
 #Isolate features & target
-X = btc_ml_df.drop(target, axis = 1)
-y = btc_ml_df[target]
+X = btc_ml_df_final.drop(target, axis = 1)
+y = btc_ml_df_final[target]
 
 #Train/test split - same split but mostly used for training and insight to performance
 X_train = X[0:round(len(X) * .66)]
